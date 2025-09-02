0e1a43c1d5c2496e466b499110911f801527f913
Luke Stamper <luke.stamper12@outlook.com>
2025-09-02 16:46:48 -0600

    Initial changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,11 @@
-// This will be our first javascript file
+//@ts-check
+
+let two = 2;
+let three = 3;
+
+if (two >= three) {
+    console.log("Math be broken");
+}
+else {
+    console.log("The universe is right again");
+}
---
+++ scripts/variables.js
@@ -0,0 +1,22 @@
+//@ts-check
+
+// Arrays
+let snacks = ["oreos", "takis", "lays"];
+
+// console.log(snacks[1], snacks.length);
+
+// Objects - this is grouped data/info
+let profile = {
+    username: "BiterBeater163",
+    favGame: "Celeste",
+    likes: 500,
+    subscribers: 25000,
+    friends: []
+}
+
+console.log(profile);
+
+profile.likes = 1216;
+
+console.log(profile.likes);
+
