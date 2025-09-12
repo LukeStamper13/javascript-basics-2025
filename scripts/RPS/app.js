dc3c0c343bb41c1cd01c04b74c8af96311493422
Luke Stamper <luke.stamper12@outlook.com>
2025-09-12 16:24:46 -0600

    added styling

--- scripts/RPS/app.js
+++ scripts/RPS/app.js
@@ -12,6 +12,11 @@ const ROCK = "rock";
 const PAPER = "paper";
 const SCISSORS = "scissors";
 
+let timesPlayed = 0;
+let tieCount = 0;
+let playerScore = 0;
+let computerScore = 0;
+
 /** @type { HTMLElement } */
 //@ts-ignore checking for null below
 let aftermathElement = document.getElementById("aftermath");
@@ -19,20 +24,68 @@ if (aftermathElement == null) {
     throw "aftermath is not defined! Check your HTML!!";
 }
 
+/** @type { HTMLElement } */
+//@ts-ignore checking for null below
+let playerWinsElement = document.getElementById("playerWins");
+if (playerWinsElement == null) {
+    throw "playerWins is not defined! Check your HTML!!";
+}
+
+/** @type { HTMLElement } */
+//@ts-ignore checking for null below
+let computerWinsElement = document.getElementById("computerWins");
+if (computerWinsElement == null) {
+    throw "computerWins is not defined! Check your HTML!!";
+}
+
+/** @type { HTMLElement } */
+//@ts-ignore checking for null below
+let timesTiedElement = document.getElementById("timesTied");
+if (timesTiedElement == null) {
+    throw "timesTied is not defined! Check your HTML!!";
+}
+
+/** @type { HTMLElement } */
+//@ts-ignore checking for null below
+let totalPlaysElement = document.getElementById("totalPlays");
+if (totalPlaysElement == null) {
+    throw "totalPlays is not defined! Check your HTML!!";
+}
+
 const pickWeapon = function (weapon) {
     let aftermathText = `Player picked ${weapon}. `;
-    console.log("Player picked: ", weapon);
 
     let computerWeapon = computerPickWeapon();
-    console.log("Computer picked: ", computerWeapon);
     aftermathText = aftermathText + `Computer picked ${computerWeapon}. `
 
     // get results from decideResults()
     let results = decideResults(weapon, computerWeapon);
-    console.log("Battle results: ", results);
 
+    let winner = "";
     if (results.isTie) {
+        winner = results.description;
+        tieCount += 1;
     }
+    else if (results.playerWon) {
+        winner = "Player wins.";
+        playerScore += 1;
+    }
+    else {
+        winner = "Computer wins!";
+        computerScore += 1;
+    }
+    aftermathText = aftermathText + `${winner} Because ${results.description}`;
+    timesPlayed += 1;
+
+    aftermathElement.textContent = aftermathText;
+    let playerWinsText = "Player has won " + (playerScore.toString()) + " times.";
+    playerWinsElement.textContent = playerWinsText;
+    let computerWinsText = "Computer has won " + (computerScore.toString()) + " times.";
+    computerWinsElement.textContent = computerWinsText;
+    let timesTiedText = "You have tied " + (tieCount.toString()) + " times.";
+    timesTiedElement.textContent = timesTiedText;
+    let totalPlaysText = "You have played " + (timesPlayed.toString()) + " times.";
+    totalPlaysElement.textContent = totalPlaysText;
 }
 
 const computerPickWeapon = function () {
